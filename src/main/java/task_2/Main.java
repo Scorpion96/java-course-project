e239979ba078a9063b2b55bc7fd99710dc6c477e
Scorpion96 <Nikitos96@ukr.net>
2020-02-12 17:08:56 +0200

    Add files Controller, Main, Model and View

---
+++ src/main/java/task_2/Main.java
@@ -0,0 +1,14 @@
+package task_2;
+
+/**
+ * Created by user on 11.02.2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller();
+
+
+    }
+}
---
+++ src/main/java/task_2/View.java
@@ -0,0 +1,7 @@
+package task_2;
+
+/**
+ * Created by user on 11.02.2020.
+ */
+public class View {
+}
